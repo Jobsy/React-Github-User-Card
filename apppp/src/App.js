61be9275bddcc621e827d22584c62e67465c1559
Oluwajoba <oluwajobabello@gmail.com>
2019-10-02 02:20:58 +0100

    declare components states / App.js

--- apppp/src/App.js
+++ apppp/src/App.js
@@ -5,16 +5,20 @@ import './App.css';
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      users: {},
+      followers: []
+    };
   }
 
   render() {
     return (
       <div>
         <h1>Hello World</h1>
-            </div>
+      </div>
     );
   }
-  
+
 }
 
 export default App;
